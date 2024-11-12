8b31b46d0d39ab44067bfdfe07fd8a40de6cd188
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2024-11-12 11:17:13 +0100

    💥 Upgrade to Kotlin 2.0.20, Koin 4.0 and Koin Annotation 2.0.0-Beta1

--- composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/data/IdGenerator.kt
+++ composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/data/IdGenerator.kt
@@ -3,6 +3,7 @@ package com.jetbrains.kmpapp.data
 import org.koin.core.annotation.Factory
 import org.koin.core.annotation.InjectedParam
 import org.koin.mp.KoinPlatformTools
+import org.koin.mp.generateId
 
 @Factory
 class IdGenerator(@InjectedParam private val prefix : String) {
